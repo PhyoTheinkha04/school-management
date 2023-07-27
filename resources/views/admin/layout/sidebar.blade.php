8c89907451ca2b1a3673fa9caf35425e03de9f53
PhyoTheinkha04 <kokha042@gmail.com>
2023-07-27 11:26:07 +0630

    ptk

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -23,7 +23,7 @@
                     <li><a href="{{ url('admin/category') }}">Category</a></li>
                     <li><a href="{{ url('admin/subcategory') }}">Sub-category</a></li>
                     <li><a href="{{ url('admin/levels') }}">Level</a></li>
-
+                    <li><a href="{{ url('admin/interests') }}">Interest</a></li>
                 </ul>
 
             </li>
